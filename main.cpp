30f3064a474b7fd3e27f373cfa5302648164bed8
biggeb4 <biggeb4@gmail.com>
2016-09-05 22:54:14 +0200

    main pronto basta aggiungere le chiamate hai metodi di ConvolutionMatrix

--- main.cpp
+++ main.cpp
@@ -12,17 +12,50 @@ int main(int argc, char *argv[]){
     cout <<command;
     BmpImage img=BmpImage(argv[1]);
     if(command=="blur") {
+        if(argv[3]=="1"){
+
+        }else {
+            if (argv[3] == "2") {
+
+            } else {
+                cout<< "invalid argument for command blur"<<endl;
+            }
+        }
 
     }else {
         if(command=="sharpen") {
+            if(argv[3]=="1"){
+
+            }else {
+                if (argv[3] == "2") {
+
+                } else {
+                    cout<< "invalid argument for command sharpen"<<endl;
+                }
+            }
 
         }else {
             if(command=="edgedetection") {
+                if(argv[3]=="1"){
+
+                }else{
+                    if(argv[3]=="2"){
+
+                    }else{
+                        if(argv[3]=="3"){
+
+                        }else{
+                            cout<< "invalid argument for command edgedetection"<<endl;
+                        }
+                    }
+                }
 
             }else {
                 if(command=="mirror"){
                     Mirror::applyMirroring(img);
                     img.save(argv[3]);
+                }else{
+                    cout <<"unknown command: "<<command <<endl;
                 }
             }
         }
