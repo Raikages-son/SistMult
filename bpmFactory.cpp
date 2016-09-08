fad2a469890c45ab4b48817fafec9fbf82352e06
biggeb4 <biggeb4@gmail.com>
2016-09-08 19:05:16 +0200

    added factory and implemented methods with bmpImage

---
+++ bpmFactory.cpp
@@ -0,0 +1,5 @@
+//
+// Created by biggeb4 on 08/09/16.
+//
+
+#include "bpmFactory.h"
---
+++ bpmFactory.h
@@ -0,0 +1,14 @@
+//
+// Created by biggeb4 on 08/09/16.
+//
+
+#ifndef SISTMULT_BPMFACTORY_H
+#define SISTMULT_BPMFACTORY_H
+
+
+class bpmFactory {
+
+};
+
+
+#endif //SISTMULT_BPMFACTORY_H
